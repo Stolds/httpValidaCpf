17ea593a0463afd06953f2a95444ae23cd4b1f2a
Stolds <calebestodulski@gmail.com>
2025-02-02 14:01:20 -0300

    adicionado função de validar CPF

--- fnCpfValidator.cs
+++ fnCpfValidator.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -14,22 +15,63 @@ namespace serverlessCpfValidation
     {
         [FunctionName("fnCpfValidator")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
+            log.LogInformation("Iniciando a validação do CPF.");
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            if(data == null)
+            {
+                return new BadRequestObjectResult("Por favor, informe o CPF.");
+            }
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            string cpf = data?.cpf;
+
+            if(ValidarCPF(cpf) == false)
+            {
+                return new BadRequestObjectResult("CPF inválido.");
+            }
+
+            string responseMessage = "CPF válido!";
 
             return new OkObjectResult(responseMessage);
+
+            
+        }
+
+        public static bool ValidarCPF(string cpf)
+        {
+            cpf = new string(cpf.Where(char.IsDigit).ToArray());
+
+            if (cpf.Length != 11)
+                return false;
+
+            if (cpf.Distinct().Count() == 1)
+                return false;
+
+            int soma = 0;
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(cpf[i].ToString()) * (10 - i);
+
+            int resto = soma % 11;
+            int digito1 = resto < 2 ? 0 : 11 - resto;
+
+            if (digito1 != int.Parse(cpf[9].ToString()))
+                return false;
+
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(cpf[i].ToString()) * (11 - i);
+
+            resto = soma % 11;
+            int digito2 = resto < 2 ? 0 : 11 - resto;
+
+            if (digito2 != int.Parse(cpf[10].ToString()))
+                return false;
+
+            return true;
         }
     }
 }
